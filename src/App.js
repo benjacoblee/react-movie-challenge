1e75bb16c65a0939a44cff6194cb1d24f22acb68
benn <benjacoblee@gmail.com>
2020-09-16 13:04:35 +0800

    feat: include axios for data fetching, able to display movie names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-    return <div className="App">Hello World</div>;
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        const getMovies = async () => {
+            let result = await axios.get(
+                "https://sometimes-maybe-flaky-api.gdshive.io/"
+            );
+
+            setMovies(result.data);
+        };
+
+        getMovies();
+    }, []);
+    return (
+        <div className="App">
+            {movies.map((movie) => {
+                const {
+                    name,
+                    productionYear,
+                    genre,
+                    synopsisShort,
+                    image
+                } = movie;
+                return (
+                    <div>
+                        <p>{name}</p>
+                        <p>{image}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
