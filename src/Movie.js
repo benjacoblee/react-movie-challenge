60c5326c4d1153839d51f32d8e4b150bcc3f33d3
benn <benjacoblee@gmail.com>
2020-09-16 16:57:42 +0800

    feat: created movie page. able to display movie data

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,77 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Movie = () => {
+    const [movie, setMovie] = useState({});
+    const [errorMessage, setErrorMessage] = useState("");
+    const location = useLocation();
+    const pathArr = location.pathname.split("/");
+    const movieName = pathArr[2];
+    const movieObjEmpty = Object.keys(movie).length === 0;
+
+    useEffect(() => {
+        const getMovies = async () => {
+            try {
+                let result = await axios.get(
+                    "https://sometimes-maybe-flaky-api.gdshive.io/"
+                );
+
+                result = result.data.reduce((acc, currVal) => {
+                    if (currVal.name === movieName) {
+                        acc = currVal;
+                    }
+
+                    return acc;
+                }, {});
+
+                setMovie(result);
+            } catch (error) {
+                setErrorMessage(
+                    error.message + ", please try refreshing the browser!"
+                );
+            }
+        };
+
+        getMovies();
+    }, []);
+
+    function createMarkup(synopsis) {
+        return { __html: synopsis };
+    }
+
+    const { name, productionYear, genre, synopsis } = movie;
+    if (movieObjEmpty) {
+        return (
+            <Fragment>
+                {errorMessage ? (
+                    <div className="container">
+                        <div className="mt-3 text-center ">{errorMessage}</div>
+                    </div>
+                ) : (
+                    <div className="alert alert-info">
+                        Loading movie, please wait..
+                    </div>
+                )}
+            </Fragment>
+        );
+    }
+
+    return (
+        <div className="container">
+            <div className="mt-2">
+                {errorMessage}
+                <h2>{name}</h2>
+                <p>{productionYear}</p>
+                <p>{genre}</p>
+                <div
+                    className="mb-5"
+                    dangerouslySetInnerHTML={createMarkup(synopsis)}
+                ></div>
+            </div>
+        </div>
+    );
+
     return <div className="container">Movie details go here</div>;
 };
 
