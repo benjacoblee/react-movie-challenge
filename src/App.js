83a99aa97c659299cf47120defb1cd4899154d69
benn <benjacoblee@gmail.com>
2020-09-16 13:48:29 +0800

    style: include bootstrap, show movies in a grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Layout from "./components/Layout";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -16,21 +17,47 @@ function App() {
     }, []);
     return (
         <div className="App">
-            {movies.map((movie) => {
-                const {
-                    name,
-                    productionYear,
-                    genre,
-                    synopsisShort,
-                    image
-                } = movie;
-                return (
-                    <div>
-                        <p>{name}</p>
-                        <p>{image}</p>
+            <Layout>
+                <div className="container">
+                    <div className="row mt-3">
+                        {movies.map((movie) => {
+                            const {
+                                name,
+                                productionYear,
+                                genre,
+                                synopsisShort,
+                                image
+                            } = movie;
+                            return (
+                                <div className="col-6 mb-3">
+                                    <div
+                                        className="card d-flex justify-content-between"
+                                        style={{
+                                            width: "18rem",
+                                            height: "300px"
+                                        }}
+                                    >
+                                        <img
+                                            src="..."
+                                            className="card-img-top"
+                                            alt="..."
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">
+                                                {name}
+                                            </h5>
+                                            <p className="card-text">
+                                                {synopsisShort}
+                                            </p>
+                                            <a href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
-                );
-            })}
+                </div>
+            </Layout>
         </div>
     );
 }
