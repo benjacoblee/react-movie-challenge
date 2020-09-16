7ac76127ea8219d16b40dbe3ed54e372bea9273d
benn <benjacoblee@gmail.com>
2020-09-16 19:36:48 +0800

    style: display more data, minor styling

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,7 +8,19 @@ const MovieCard = ({
         <div className="col-6 mb-3">
             <div className="card d-flex justify-content-between">
                 <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
+                    <h5 className="card-title">
+                        {name}
+                        <span className="ml-1">
+                            <small className="font-weight-light">
+                                {productionYear}
+                            </small>
+                        </span>
+                    </h5>
+                    <p>
+                        <span className="badge badge-pill badge-light">
+                            {genre}
+                        </span>
+                    </p>
                     <p className="card-text">{synopsisShort}</p>
                     <Link to={`/movies/${name}`}>Read More</Link>
                 </div>
